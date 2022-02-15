631a28a68a7f83efd243763ae41cbb28bcad61ef
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-02-15 14:09:12 +0100

    created add, update, read functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get "tasks", to: "tasks#index"
+# add new tasks: 1. new form
+get "tasks/new", to: "tasks#new", as: :new
+# add new tasks: 2. post the form
+post "tasks", to: "tasks#create"
+
+get "tasks/:id", to: "tasks#show", as: :task
+
+
+# edit task 1. get the form
+get "tasks/:id/edit", to: "tasks#edit", as: :edit
+# edit task 1. patch the form
+patch "tasks/:id", to: "tasks#update"
 end
